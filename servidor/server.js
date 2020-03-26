61e648668356c8648325aba75ca16b8171e4c694
Gabinorf <gabinorf@gmail.com>
2020-03-25 21:53:56 -0300

    server

---
+++ servidor/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const server = express();
+const cors = require('cors');
+
+server.use(cors());
+server.use(bodyParser.json());
+
+
+let usuarios = [
+    {
+        nombre : "gabino",
+        contresena:"rodriguez",
+        mail: "gabino@gmail.com",
+        articulos: [
+            {
+                nombre: "pelota",
+                descripciones: "pelota de futbol usada en el Maracana",
+                estado: "en stock" 
+            },
+            {
+                nombre: "gafas",
+                descripciones: "fueron usadas por la Coca Sarli",
+                estado: "sin stock" 
+            }
+        ]
+    }
+    {
+        nombre : "micaela",
+        contresena:"rosas",
+        mail: "mica@gmail.com",
+        articulos: [
+            {
+                nombre: "jeans",
+                descripciones: "tienen un guraco",
+                estado: "en stock" 
+            },
+            {
+                nombre: "alcohol en gel",
+                descripciones: "con aloe vera",
+                estado: "sin stock" 
+            }
+        ]
+    }
+];
+
+
+server.listen(3000, () => {
+    console.log("se ha iniciado el server");
+});
+
+
+server.get('/usuarios', (req,res) =>{
+    res.send(usuarios);
+});
+
+
+server.post("/usuarios",(req,res) =>{
+    usuarios.push(req.body);
+    res.json("se agrego un usuario nuevo");
+});
+
+
